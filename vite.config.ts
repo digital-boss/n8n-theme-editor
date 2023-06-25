649e2b22b4984406417c6b13d2cafa4084c440cb
perseus-algol <129220578+perseus-algol@users.noreply.github.com>
2023-06-25 15:21:13 +0300

    iframe with n8n

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
@@ -8,7 +8,10 @@ export default defineConfig({
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    devtools(),
+    // devtools({
+    //   /* features options - all disabled by default */
+    //   autoname: true, // e.g. enable autoname
+    // }),
     solidPlugin(),
   ],
   server: {
